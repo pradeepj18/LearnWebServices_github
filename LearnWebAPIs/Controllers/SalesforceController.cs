314240ec6b67bdf25f155b02177318b7496e1dd3
pradeepj18 <31722514+pradeepj18@users.noreply.github.com>
2018-07-26 14:14:58 +0530

    Add files via upload

---
+++ LearnWebAPIs/Controllers/SalesforceController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace LearnWebAPIs.Controllers
+{
+    
+    [Produces("application/json")]
+    [Route("api/Salesforce")]
+    public class SalesforceController : Controller
+    {
+        private const string LOGIN_ENDPOINT = "https://login.salesforce.com/services/oauth2/token";
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string AuthToken { get; set; }
+        public string InstanceUrl { get; set; }
+
+        [HttpGet("login")]
+        [Produces("application/json")]
+        public string Login()
+        {
+            String jsonResponse;
+            using (var client = new HttpClient())
+            {
+                var request = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    {"grant_type", "password"},
+                    {"client_id", "3MVG9d8..z.hDcPLMwnhCIndM36TjVbIGMyNbyFU8obBz_IVqYsDe9NuFbCmp3M98YHhfjra2ibpqmlB1.m2d"},
+                    {"client_secret", "3364958416727039608"},
+                    {"username", "info.mca2016@gmail.com"},
+                    {"password", "info.salesforce2020fJO7xUW9mFoHoW7NZ55024iAa"}
+                }
+                );
+                request.Headers.Add("X-PrettyPrint", "1");
+                var response = client.PostAsync(LOGIN_ENDPOINT, request).Result;
+                jsonResponse = response.Content.ReadAsStringAsync().Result;
+            }
+            var values = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonResponse);
+             AuthToken = values["access_token"];
+             InstanceUrl = values["instance_url"];
+            using (var client = new HttpClient())
+            {
+                string restRequest = InstanceUrl + "/services/data/v42.0/" + "query/?q=select Name from Account limit 1";
+                var request = new HttpRequestMessage(HttpMethod.Get, restRequest);
+                request.Headers.Add("Authorization", "Bearer " + AuthToken);
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                request.Headers.Add("X-PrettyPrint", "1");
+                var response = client.SendAsync(request).Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+ 
+        }
+        
+    }
+}
---
+++ LearnWebAPIs/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnWebAPIs.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        static List<string> strlist = new List<string>() {
+            "String 1",
+            "String 2",
+            "String 3",
+            "String 4"
+        };
+        // GET api/values
+        [HttpGet]
+        [Produces("application/json")]
+        public IEnumerable<string> Get()
+        {
+            return strlist;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get([FromQuery]int id,string text)
+        {
+            return Ok(new Val { Id = id,Text=text});
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Val value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return CreatedAtAction("Get", new { id = value.Id, text = value.Text });
+           // strlist.Add(value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+            strlist[id] = value;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            strlist.RemoveAt(id);
+        }
+    }
+    public class Val
+    {
+        public int Id { get; set; }
+        [MinLength(3)]
+        public string Text { get; set; }
+    }
+}
